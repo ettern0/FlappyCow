93eae56b21ab7fc8ea8238d08b8308656c9e78ab
ettern0 <evserdyukov@gmail.com>
2022-04-06 10:29:54 +0200

    Initial Commit

---
+++ PushGame/PushGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushGameApp.swift
+//  PushGame
+//
+//  Created by Сердюков Евгений on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PushGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
